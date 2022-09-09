362c880a0c035722f935c5b6eb31e587ec71d2b6
Mishak Mosimabale <mishakmanuel@gamil.com>
2022-09-09 06:53:27 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - entry point of the program
+  *
+  * Takes no arguments
+  *
+  * Return: program always returns 0(success)
+  */
+int main(void)
+{
+	char nbase_16 i;
+
+	for (nbase_16 = 48; nbase_16 <= 57; nbase_16++)
+	{
+		putchar(nbase_16);
+	}
+	putchar('\n');
+
+	return (0);
+}
