450416d8c26d0a09eaeeed6fdbf11997fc8f5543
Mishak Mosimabale <mishakmanuel@gamil.com>
2022-09-09 04:31:00 -0700

    task1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,20 @@
 #include <time.h>
 /* more header's goes there */
 
-/* betty style Description of function here*/
+/**
+  * main- Entry point of th progam
+  * program takes no arg.
+  *
+  * Description - uses if else to determine if an int is nagative or positve
+  *
+  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d: is positive\n" , n);
