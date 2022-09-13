2439cc116ab9b47383f9f5ae4364824b29474e6b
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-13 14:09:23 +0000

    Task3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,10 +10,10 @@
   */
 void _islower(int c)
 {
-	if (c >= 97)
+	if ((c >= 97) && (c <= 122))
 		return (1);
-	else if (c <= 122)
-		return (1)
+	else if ((c >= 65) && (c <= 90))
+		return (1);
 	else
 		return (0);
 }
