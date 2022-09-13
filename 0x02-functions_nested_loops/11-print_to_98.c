f7d76b03d18bd9800d795de5d6548997dacb377d
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-13 18:59:53 +0000

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_to_98 - is a function that checks if character alphabet
+  *
+  * @n: takes in int c and checks
+  *
+  * Return: Always return 0 on success
+  *
+  */
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+	{
+		printf("%d, " n);
+}
