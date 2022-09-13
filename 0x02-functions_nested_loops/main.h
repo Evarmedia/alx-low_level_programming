0b4c64710fb637e5c3f3b9c620428d499474392e
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-13 14:25:52 +0000

    updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,16 +2,25 @@
 #define MAIN_H_INCLUDED
 
 /**
-  * File : main.h - header file for the program
+  * File: main.h - header file for the program
   *
+  * Auth - Mosi
   * Desc - contains all functions used
   * Return: Always returns 0(success)
-  * On error, -1 is returned, and err is set appropriately.
   */
 int _putchar(char c);
 int main(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
