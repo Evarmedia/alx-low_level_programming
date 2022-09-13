e75064d82fd38a480cd06fbd963d69aa22a3e556
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-13 19:52:07 +0000

    task11

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * times_table - display times table
+  *
+  * @n: number parameter
+  *
+  *Return: Returns multiplicaation table starting from 0
+  */
+void times_table(void)
+{
+
+}
