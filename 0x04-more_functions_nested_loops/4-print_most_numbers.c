1ba6ee83182c764713f11d24cb8a0b7e2856854e
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-15 12:58:28 +0000

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_numbers - function that prints the aphpabets
+  *
+  * Return: Always return 0 on success
+  *
+  */
+void print_numbers(void)
+{
+	char num;
+
+	num = 48;
+	for (num = 48; num < 58; num++)
+	{
+		_putchar(num);
+		if (num == 50 || num == 52)
+			continue;
+		num++;
+	}
+	_putchar(10);
+}
