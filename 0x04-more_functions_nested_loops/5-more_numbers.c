65389ef9c7cbed9b3f2c785f606e9d90721ee87c
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-15 13:56:48 +0000

    tas5a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_numbers - function that prints the aphpabets
+  *
+  * Return: Always return 0 on success
+  *
+  */
+void print_numbers(void)
+{
+	int num, i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		num = 0;
+		while (num < 15)
+		{
+			_putchar('0' + num);
+			num++;
+		}
+	}
+	_putchar(10);
+}
