01cff23bff81115f261056737a7751ab56a948ee
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-15 19:24:41 +0000

    Advtask

--- 0x04-more_functions_nested_loops/test/101-print_numberc.c
+++ 0x04-more_functions_nested_loops/test/101-print_numberc.c
@@ -8,15 +8,14 @@
 
 void print_number(int n)
 {
-	unsigned int x;
-
-	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		x = -n;
+		n = -n;
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
