a4bdcb8334726d5c43cba558a35ca2b3ee402d10
Mishak Mosi <mishakmanuel@gmail.com>
2022-09-22 16:58:40 +0000

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
 
-		for (j = 0; j < n && src[i] != '\0'; j++)
+		for (j = 0; j < n && src[j] != '\0'; j++)
 			dest[i] = src[j];
 		for ( ; j < n; j++)
 			dest[j] = '\0';
