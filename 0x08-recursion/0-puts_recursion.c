03bf59cfa936adacbbf930fe685c8135a42d1ef5
Evarmedia <mishakmanuel@gmail.com>
2023-07-05 08:41:19 +0000

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+/**
+  * _print_rev_recursion - print in reverse using recursion.
+  *
+  * @s: pointer parameter that takes char
+  * Description: Recursion, a function that prints a string in reverse.
+  * Return: Reveresed string
+  *
+  */
+int void _print_rev_recursion(char *s)
+{
+}
