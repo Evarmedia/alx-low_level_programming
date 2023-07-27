069e0da358592b75af31f97f44899fe5928c19b9
Evarmedia <mishakmanuel@gmail.com>
2023-07-27 16:58:45 +0000

    task1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_node - adds a new node at the beginning
+ * add_node - adds new node to the head
  * of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
