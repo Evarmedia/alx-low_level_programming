9da66fcd068e9e3640247a342259972ab7d27466
Evarmedia <mishakmanuel@gmail.com>
2024-02-08 17:17:32 +0100

    fixxs

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,40 +11,40 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t cur_pos, low_down, up_high;
-	double fig;
+	size_t pos, low, high;
+	double f;
 
 	if (array == NULL)
 		return (-1);
 
-	low_down = 0;
-	up_high = size - 1;
+	low = 0;
+	high = size - 1;
 
 	while (size)
 	{
-		fig = (double)(up_high - low_down) / (array[up_high] - array[low_down]) * (value - array[low_down]);
-		cur_pos = (size_t)(low_down + fig);
-		printf("Value checked array[%d]", (int)cur_pos);
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
 
-		if (cur_pos >= size)
+		if (pos >= size)
 		{
 			printf(" is out of range\n");
 			break;
 		}
 		else
 		{
-			printf(" = [%d]\n", array[cur_pos]);
+			printf(" = [%d]\n", array[pos]);
 		}
 
-		if (array[cur_pos] == value)
-			return ((int)cur_pos);
+		if (array[pos] == value)
+			return ((int)pos);
 
-		if (array[cur_pos] < value)
-			low_down = cur_pos + 1;
+		if (array[pos] < value)
+			low = pos + 1;
 		else
-			up_high = cur_pos - 1;
+			high = pos - 1;
 
-		if (low_down == up_high)
+		if (low == high)
 			break;
 	}
 
